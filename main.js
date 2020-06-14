fe9d59bd181d4703ac07358051e331e93b20ee0f
Christina Zhang <c.zhang51@gmail.com>
2020-06-14 12:05:08 -0700

    attempt at a single quote

---
+++ main.js
@@ -0,0 +1,7 @@
+var compassion_quote = “For there is nothing heavier than compassion. 
+	Not even one's own pain weighs so heavy as the pain one feels with someone, 
+	for someone, a pain intensified by the imagination and prolonged by a hundred echoes.” 
+var compassion_author = "Milan Kundera"
+var compassion_work = "The Unbearable Lightness of Being"
+
+document.getElementById("quote").innerHTML = compassion_quote;
